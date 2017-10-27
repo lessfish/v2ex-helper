7f39303fefed13018d87c14279065c730587f633
hanzichi <hanzichi@outlook.com>
2017-10-27 23:19:27 +0800

    修复 chrome.storage.* 使用错误

--- content-scripts/signin/signin.js
+++ content-scripts/signin/signin.js
@@ -12,7 +12,7 @@ chrome.runtime.sendMessage({
     const todayStr = [year, month, day].join('-')
 
     // 读取数据，第一个参数是指定要读取的 key 以及设置默认值
-    chrome.storage.sync.get({lastSigninDate: ''}, function(items) {
+    chrome.storage.sync.get(['lastSigninDate'], function(items) {
       if (items.lastSigninDate === todayStr) return // 今天已经签到过了
 
       // 保存数据
